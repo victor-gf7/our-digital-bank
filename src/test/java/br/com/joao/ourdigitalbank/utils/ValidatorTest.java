c282be13af9ef6b985d86e7276283215d9f8b659
unknown <ferreirazaltan@gmail.com>
2021-03-21 12:03:25 -0300

    refatoração testes

--- src/test/java/br/com/joao/ourdigitalbank/utils/ValidatorTest.java
+++ src/test/java/br/com/joao/ourdigitalbank/utils/ValidatorTest.java
@@ -1,14 +1,58 @@
 package br.com.joao.ourdigitalbank.utils;
 
-import org.junit.jupiter.api.Assertions;
+import br.com.joao.ourdigitalbank.model.client.Client;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ValidatorTest {
 
     @Test
-    void validateFormClient() {
+    @DisplayName("Deveria validar email, cpf e idade maior de 18 anos")
+    void test1() {
+        Client client = new Client();
+        client.setEmail("ferreirazaltan@gmail.com");
+        client.setCpf("449.395.810-39");
+        client.setDateBirth(LocalDate.of(1998,7,28));
+
+        assertTrue(Validator.validateFormClient(client));
+    }
+
+    @Test
+    @DisplayName("Não deveria passar email inválido, cpf e idade menor que 18 anos")
+    void test2() {
+        Client client = new Client();
+        client.setEmail("ferreirazaltangmail.com");
+        client.setCpf("449.395.810-39");
+        client.setDateBirth(LocalDate.of(1998,7,28));
+
+        assertFalse(Validator.validateFormClient(client));
+    }
+
+    @Test
+    @DisplayName("Não deveria passar email inválido, cpf invalido e idade menor que 18 anos")
+    void test3() {
+        Client client = new Client();
+        client.setEmail("ferreirazaltangmail.com");
+        client.setCpf("111.111.111-11");
+        client.setDateBirth(LocalDate.of(1998,7,28));
+
+        assertFalse(Validator.validateFormClient(client));
+    }
+
+    @Test
+    @DisplayName("Não deveria passar email inválido, cpf invalido e idade menor que 18 anos")
+    void test4() {
+        Client client = new Client();
+        client.setEmail("ferreirazaltangmail.com");
+        client.setCpf("111.111.111-11");
+        client.setDateBirth(LocalDate.of(2010,7,28));
+
+        assertFalse(Validator.validateFormClient(client));
     }
 
     @Test
@@ -27,9 +71,12 @@ class ValidatorTest {
     }
 
     @Test
-    void isValidAge() {
-        String validAge = "28/07/1998";
+    @DisplayName("Deveria validar idade maior que 18")
+    void test5() {
+        LocalDate birthDate = LocalDate.of(2010, 7, 28);
+        LocalDate birthDateGreaterThan18 = LocalDate.of(1998, 7, 28);
 
-        //assertTrue(Validator.isValidAge(validAge));
+        assertTrue(Validator.isValidAge(birthDateGreaterThan18));
+        assertFalse(Validator.isValidAge(birthDate));
     }
 }
